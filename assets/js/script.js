bfd151a4bceb55a48c400e077bfc6bfeb1c5c445
sunny kavar <kavarsunny51@gmail.com>
2025-04-25 18:46:06 +0530

    responsive changes

--- assets/js/script.js
+++ assets/js/script.js
@@ -161,17 +161,15 @@ VanillaTilt.init(document.querySelectorAll(".tilt"), {
 // <!-- tilt js effect ends -->
 
 
-// pre loader start
-// function loader() {
-//     document.querySelector('.loader-container').classList.add('fade-out');
-// }
-// function fadeOut() {
-//     setInterval(loader, 500);
-// }
-// window.onload = fadeOut;
-// pre loader end
 
-// disable developer mode
+function loader() {
+    document.querySelector('.loader-container').classList.add('fade-out');
+}
+function fadeOut() {
+    setInterval(loader, 500);
+}
+window.onload = fadeOut;
+
 document.onkeydown = function (e) {
     if (e.keyCode == 123) {
         return false;
